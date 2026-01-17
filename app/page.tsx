fd085a3cd3905544072edd76aa77a7754a7f2c76
Mahi <hero90960@gmail.com>
2026-01-17 13:51:10 +0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    
+    <div>
+      <h1>Welcome to Ai Video Generator</h1>
+      <Button className="mt-4">Get Started</Button>
+    </div>
+  );
+}
