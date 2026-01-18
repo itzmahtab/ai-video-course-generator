3f77641c9e38d3205fb98dc38d39a2cdf709afc5
Mahi <hero90960@gmail.com>
2026-01-18 12:12:19 +0600

    authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Provider from "./provider";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,19 +32,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Provider>
+              {children}
+              </Provider>
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ app/provider.tsx
@@ -0,0 +1,30 @@
+"use client"
+
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { UserDeatailContext } from '@/UserDetailContext'
+
+function Provider({children}: {children: React.ReactNode}) {
+  const [userDetail,setUserDetail]=useState(null)
+
+    useEffect(() => {
+      
+    CreateNewUser()
+    }, [])
+    
+    const CreateNewUser=async()=> {
+        //user api endpoint call to create user in db
+        const result = await axios.post('/api/user', {})
+        console.log(result.data)
+        setUserDetail(result.data)
+    }
+  return (
+    <div>
+      <UserDeatailContext.Provider value={{userDetail,setUserDetail}}>
+      {children}
+      </UserDeatailContext.Provider>
+    </div>
+  )
+}
+
+export default Provider
